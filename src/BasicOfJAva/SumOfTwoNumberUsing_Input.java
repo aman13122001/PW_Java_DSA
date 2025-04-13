aac0d1770d133897663efdc5431ea81ca70d39bc
aman13122001 <amankumar13122001@gmail.com>
2025-04-13 19:11:47 +0530

    Sum Of Two Number Using Input

---
+++ src/BasicOfJAva/SumOfTwoNumberUsing_Input.java
@@ -0,0 +1,16 @@
+package BasicOfJAva;
+import java.util.Scanner;
+
+public class SumOfTwoNumberUsing_Input {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter First Number:");
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+
+        int Sum = num1+num2;
+
+        System.out.println("the Sum of "+num1+" and "+num2+" = "+Sum);
+
+    }
+}
