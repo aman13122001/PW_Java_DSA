ce3b71ef45010dce3883f55984e7d5988a3c0e44
aman13122001 <amankumar13122001@gmail.com>
2025-04-13 16:08:36 +0530

    Add Double Arithmetic Operation

---
+++ src/BasicOfJAva/Arithmetic_OPer_On_Double_Date_type.java
@@ -0,0 +1,13 @@
+package BasicOfJAva;
+
+public class Arithmetic_OPer_On_Double_Date_type
+{
+    public static void main(String[] args) {
+        double x = 5;
+        double y = 2;
+        System.out.println(x+y);
+        System.out.println(x-y);
+        System.out.println(x*y);
+        System.out.println(x/y);
+    }
+}
