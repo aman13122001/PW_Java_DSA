be439f19c6df95b59b9031eacee1a3bb42a674ed
aman13122001 <amankumar13122001@gmail.com>
2025-04-13 16:16:55 +0530

    percentage of 5 Subjects

---
+++ src/BasicOfJAva/Calculating_Per_Of_5_sub.java
@@ -0,0 +1,15 @@
+package BasicOfJAva;
+
+public class Calculating_Per_Of_5_sub {
+    public static void main(String[] args) {
+        double x1 =90;
+        double x2 =91;
+        double x3 =94;
+        double x4 =93;
+        double x5 =92;
+        double y =(x1+x2+x3+x4+x5)/5;
+        System.out.println("percentage of 5 Subjects:"+y);
+
+    }
+
+}
