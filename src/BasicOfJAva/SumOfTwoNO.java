79ffd6eec358932ea2982c602563e88e01781a53
aman13122001 <amankumar13122001@gmail.com>
2025-04-13 13:22:18 +0530

    Sum of two number Program

---
+++ src/BasicOfJAva/SumOfTwoNO.java
@@ -0,0 +1,10 @@
+package BasicOfJAva;
+
+public class SumOfTwoNO {
+    public static void main(String[] args) {
+        int x=8;
+        int y=6;
+        int sum=x+y;
+        System.out.println("sum of two number is "+sum);
+    }
+}
