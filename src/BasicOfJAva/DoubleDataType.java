4c154f7b8d274dfd0cfeedc47fd95e9976235cef
aman13122001 <amankumar13122001@gmail.com>
2025-04-13 13:54:13 +0530

    Add Double Data Type Program

---
+++ src/BasicOfJAva/DoubleDataType.java
@@ -0,0 +1,30 @@
+package BasicOfJAva;
+
+public class DoubleDataType {
+    public static void main(String[] args) {
+        int x=5;
+        int y=2;
+        int z= x/y;
+        System.out.println("value of z= "+z);
+//      output : 2;
+
+        double a=5;
+        double b=2;
+        double c=a/b;
+        System.out.println("value of c="+c);
+//      The output is c = 2.5
+
+//      the number treated as integer number
+        double divide = 5/2;
+        System.out.println(divide);
+//        output: 2.0
+
+//      the number treated as integer number
+        double div = 5.0/2;
+        System.out.println(div);
+//        output: 2.5++
+
+
+
+    }
+}
