9f3d5cc3b9f05e0463943c25d3b5b1d8b4ef9831
aman13122001 <amankumar13122001@gmail.com>
2025-04-13 18:44:15 +0530

    Calculating_Simple_interest

---
+++ src/BasicOfJAva/Calculating_Simple_interest.java
@@ -0,0 +1,12 @@
+package BasicOfJAva;
+
+public class Calculating_Simple_interest {
+    public static void main(String[] args) {
+        double principal=100;
+        double rate=10;
+        double time=2;
+        double Simple_Interest=(principal*rate*time)/100;
+        System.out.println("Simple Interest : "+Simple_Interest);
+    }
+
+}
