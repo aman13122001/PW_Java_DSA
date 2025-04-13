f5ea97d2c97c480d1c911edeeb85a042cea81c71
aman13122001 <amankumar13122001@gmail.com>
2025-04-13 10:12:52 +0530

    Printing number in jav

---
+++ src/BasicOfJAva/PrintingNumber.java
@@ -0,0 +1,10 @@
+package BasicOfJAva;
+
+public class PrintingNumber {
+    public static void main(String[] args) {
+        System.out.println("4");
+        System.out.println(4);
+        System.out.println(4+5);
+        System.out.println("4+5");
+    }
+}
