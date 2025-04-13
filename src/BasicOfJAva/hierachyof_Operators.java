9361c69ce1c0fe8a246c91a241ca9a87642c23a2
aman13122001 <amankumar13122001@gmail.com>
2025-04-13 21:53:31 +0530

    Hierachy of Operators

---
+++ src/BasicOfJAva/hierachyof_Operators.java
@@ -0,0 +1,18 @@
+package BasicOfJAva;
+
+public class hierachyof_Operators {
+    public static void main(String[] args) {
+        int i=2,j=3,k,l;
+        double a,b;
+        k=i/j*j;
+        l=j/i*i;
+        a = i/j*j;
+        b=j/i*i;
+
+//      System.out.println(k+" "+l+" "+a+" "+b);
+        System.out.println("value of k :" +k);
+        System.out.println("value of l :" +l);
+        System.out.println("value of a :" +a);
+        System.out.println("value of b :" +b);
+    }
+}
