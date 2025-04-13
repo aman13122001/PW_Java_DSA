b2a5652752306415fc95db610722799f07b0c4c5
aman13122001 <amankumar13122001@gmail.com>
2025-04-13 16:53:19 +0530

    Calculate_Area_Of_Circle

---
+++ src/BasicOfJAva/Calculate_Area_Of_Circle.java
@@ -0,0 +1,10 @@
+package BasicOfJAva;
+
+public class Calculate_Area_Of_Circle {
+    public static void main(String[] args) {
+        double radius = 5;
+        double pi = 3.1415;
+        double area = pi*radius*radius;
+        System.out.println("Area of Circle : "+area);
+    }
+}
